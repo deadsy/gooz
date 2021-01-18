97ebeda622bfa65e8b456f91459d82aa11703e2c
Jason Harris <sirmanlypowers@gmail.com>
2021-01-18 12:18:19 -0800

    Provided a MIDI output callback.

--- ggm/src/os/zephyr/main.c
+++ ggm/src/os/zephyr/main.c
@@ -10,6 +10,17 @@
 #include "module.h"
 
 /******************************************************************************
+ * MIDI output callback
+ */
+
+static void midi_out(void *arg, const struct event *e, int idx) {
+	uint8_t status = e->u.midi.status;
+	uint8_t arg0 = e->u.midi.arg0;
+	uint8_t arg1 = e->u.midi.arg1;
+	LOG_DBG("midi_out[%d] %d:%d:%d", idx, status, arg0, arg1);
+}
+
+/******************************************************************************
  * main
  */
 
@@ -30,6 +41,8 @@ int main(void) {
 	if (err != 0) {
 		goto exit;
 	}
+
+	s->midi_out = midi_out;
 
 	for (int i = 0; i < 3000; i++) {
 		synth_loop(s);
